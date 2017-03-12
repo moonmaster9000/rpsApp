a3f45076bd0c49d48f84b8b40981fb0ffbb2ce8d
Matt Parker <moonmaster9000@gmail.com>
2017-03-12 16:58:51 -0400

    refactoring tests into their own component

---
+++ webSpecs/src/webSpecs.js
@@ -0,0 +1,110 @@
+const React = require("react")
+const ReactDOM = require("react-dom")
+
+const {Round} = require("rps")
+
+function webSpecs(RPSApp){
+    describe("play", function () {
+        describe("when the play use case fails validations", function () {
+            beforeEach(function () {
+                renderApp({
+                    play: function(p1, p2, ui){ ui.invalid() }
+                })
+            })
+
+            it("tells the user the input is invalid", function () {
+                expect(page()).not.toContain(INVALID)
+                play()
+                expect(page()).toContain(INVALID)
+            })
+        })
+
+        describe("when the play use case reports p1 wins", function () {
+            beforeEach(function () {
+                renderApp({
+                    play: function(p1, p2, ui){ ui.p1Wins() }
+                })
+            })
+
+            it("tells the user P1 Wins!", function () {
+                expect(page()).not.toContain(P1_WINS)
+                play()
+                expect(page()).toContain(P1_WINS)
+            })
+        })
+
+        describe("when the play use case reports p2 wins", function () {
+            beforeEach(function () {
+                renderApp({
+                    play: function(p1, p2, ui){ ui.p2Wins() }
+                })
+            })
+
+            it("tells the user P2 Wins!", function () {
+                expect(page()).not.toContain(P2_WINS)
+                play()
+                expect(page()).toContain(P2_WINS)
+            })
+        })
+
+        describe("when the play use case reports tie", function () {
+            beforeEach(function () {
+                renderApp({
+                    play: function(p1, p2, ui){ ui.tie() }
+                })
+            })
+
+            it("tells the user P2 Wins!", function () {
+                expect(page()).not.toContain(TIE)
+                play()
+                expect(page()).toContain(TIE)
+            })
+        })
+
+        describe("when the history returns results", function () {
+            beforeEach(function () {
+                renderApp({
+                    history: ui=>ui.rounds([new Round("rock", "paper", "p1")])
+                })
+            })
+
+            it("shows those rounds on the page", function () {
+                play()
+                expect(page()).toContain("rock")
+                expect(page()).toContain("paper")
+                expect(page()).toContain("p1")
+            })
+
+        })
+
+
+        function page() {
+            return document.getElementById("reactApp").innerText;
+        }
+
+        function play() {
+            document.getElementById("playButton").click()
+        }
+
+        function renderApp(useCases) {
+            useCases.history = useCases.history || function(){}
+            useCases.play    = useCases.play    || function(){}
+
+            ReactDOM.render(
+                React.createElement(RPSApp, { useCases }),
+                document.getElementById("reactApp")
+            )
+        }
+
+        const P1_WINS = "p1 wins"
+        const P2_WINS = "player two wins"
+        const TIE     = "TIE"
+        const INVALID = "INVALID"
+
+        afterEach(function () {
+            document.getElementById("reactApp").innerHTML = ""
+        })
+    })
+}
+
+module.exports = webSpecs
