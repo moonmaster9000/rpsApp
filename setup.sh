4ddc3c36c256067045cae1fc093aff0ce43da0d2
Matt Parker <moonmaster9000@gmail.com>
2017-03-30 21:35:37 -0400

    Setup script

---
+++ setup.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+DEPENDENCIES="rps rpsPresentationI18n webSpecs"
+FRONTENDS="web reduxWeb refluxWeb angularWeb"
+
+install() {
+    for dir in $1; do
+        pushd $dir
+        npm install
+        popd
+    done
+}
+
+npmLink() {
+    for dir in $1; do
+        pushd $dir
+        npm link
+        popd
+    done
+}
+
+npmLinkDependencies() {
+    for dir in $1; do
+      pushd $dir
+
+      for dep in $2; do
+        npm link $dep
+      done
+
+      popd
+    done
+}
+
+install "$DEPENDENCIES"
+install "$FRONTENDS"
+
+npmLink "$DEPENDENCIES"
+
+npmLinkDependencies "$FRONTENDS" "$DEPENDENCIES"
