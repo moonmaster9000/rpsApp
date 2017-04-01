9e88be4530e6f9d7a4958f76a3e07cf7be04499c
Matt Parker <moonmaster9000@gmail.com>
2017-03-31 22:53:41 -0400

    refactored build script

--- build.sh
+++ build.sh
@@ -1,19 +1,9 @@
-pushd rps
-node_modules/jasmine/bin/jasmine.js
-popd
+pushd rps && node_modules/jasmine/bin/jasmine.js && popd
 
-pushd web
-node_modules/karma/bin/karma start web.karma.conf.js
-popd
+FRONTENDS="web angularWeb refluxWeb reduxWeb"
 
-pushd angularWeb
-node_modules/karma/bin/karma start web.karma.conf.js
-popd
-
-pushd refluxWeb
-node_modules/karma/bin/karma start web.karma.conf.js
-popd
-
-pushd reduxWeb
-node_modules/karma/bin/karma start web.karma.conf.js
-popd
+for frontend in $FRONTENDS; do
+    pushd $frontend
+    node_modules/karma/bin/karma start web.karma.conf.js
+    popd
+done
