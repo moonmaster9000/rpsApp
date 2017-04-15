0c325c2f98daf47bec705d3876137fa4fb64e410
Matt Parker <moonmaster9000@gmail.com>
2017-04-14 21:58:37 -0400

    using firebase db in the web main.js

--- web/main.js
+++ web/main.js
@@ -1,10 +1,25 @@
 const React = require("react")
 const ReactDOM = require("react-dom")
 const RPSApp = require("./RPSApp")
+const { provideFirebaseRepoFactory } = require("firebasePersist")
 const {UseCaseFactory} = require("rps")
-const PouchRepoFactory = require("pouchPersist")
+
+function generateUUID() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+        return v.toString(16);
+    })
+}
+
+const FirebaseRepoFactory = provideFirebaseRepoFactory(
+    "AIzaSyA811T0l1_v1utC7d8DFpZcYMlNfJC9nfo",
+    "https://rpstest-f3cba.firebaseio.com",
+    function() {
+        return generateUUID()
+    }
+)
 
 ReactDOM.render(
-    <RPSApp useCases={new UseCaseFactory(new PouchRepoFactory("webRoundDatabase"))}></RPSApp>,
+    <RPSApp useCases={new UseCaseFactory(new FirebaseRepoFactory())}></RPSApp>,
     document.getElementById("app")
 )
